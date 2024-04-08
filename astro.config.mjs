5812de01320a6342b06a6800f4b29069c81cddaf
Zach McLean <zmclean11@gmail.com>
2024-04-07 21:39:11 -0500

    playing around with astro | updated site structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), tailwind(), react()]
+});
