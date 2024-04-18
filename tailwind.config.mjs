f22bbe38f761e4cc6dec802e868397e1a34b7f8e
Zach McLean <zmclean11@gmail.com>
2024-04-18 11:19:43 -0500

    Updated styling and content for home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    fontFamily: {
+        // sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+    },
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("@catppuccin/tailwindcss")],
 }
