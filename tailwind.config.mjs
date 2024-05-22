c5eaa05b4479f31561a2103a392591e7e39ba31c
Zach McLean <zmclean11@gmail.com>
2024-05-22 14:50:18 -0500

    last changes before os reset

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     fontFamily: {
-        // sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        // sans: ["B612", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
     },
-		extend: {},
+		extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      }
+    },
 	},
 	plugins: [require("@catppuccin/tailwindcss")],
 }
